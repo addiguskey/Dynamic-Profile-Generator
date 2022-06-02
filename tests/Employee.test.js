f24c51f735bc7e40b3d4103594c4c1f96bc522e2
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-06-01 22:33:49 -0600

    file updates

---
+++ tests/Employee.test.js
@@ -0,0 +1,14 @@
+const { exportAllDeclaration } = require("@babel/types");
+const Employee = require("../lib/employee");
+
+describe("Employee", () => {
+  describe("Initialization", () => {
+    it("should create a new employee obj with name, id, and email", () => {
+      const employee = new Employee("Addi", 4, "addisonguskey@gmail.com");
+
+      expect(employee.name).toEqual("Addi");
+      expect(employee.id).toEqual(4);
+      expect(employee.email).toEqual("addisonguskey@gmail.com");
+    });
+  });
+});
